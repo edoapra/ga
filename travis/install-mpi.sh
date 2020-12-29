1eb24fcb3cd327d73032d76edda09077a574fc8e
edoapra <edoardo.apra@gmail.com>
2020-12-29 11:57:34 -0800

    fix conditionals

--- travis/install-mpi.sh
+++ travis/install-mpi.sh
@@ -37,7 +37,7 @@ case "$os" in
         echo "Linux"
         case "$MPI_IMPL" in
             mpich)
-                if [ ! -d "$TRAVIS_ROOT/mpich" ] || [ ! -x "$(PATH=$TRAVIS_ROOT/mpich/bin:$PATH: command -v mpif90)" ]; then
+                if [ ! -d "$TRAVIS_ROOT/mpich" ] || [  -z "$1/bin/mpicc)" ]; then
                     wget --no-check-certificate http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
                     tar -xzf mpich-3.2.tar.gz
                     cd mpich-3.2
@@ -71,7 +71,7 @@ EOF
                 fi
                 ;;
             openmpi)
-                if [ ! -d "$TRAVIS_ROOT/open-mpi" ] || [ ! -x "$(PATH=$TRAVIS_ROOT/open-mpi/bin:$PATH: command -v mpif90)" ]; then
+                if [ ! -d "$TRAVIS_ROOT/open-mpi" ] || [  -z "$1/bin/mpicc3)" ]; then
                     wget --no-check-certificate https://www.open-mpi.org/software/ompi/v2.0/downloads/openmpi-2.0.2.tar.bz2
                     tar -xjf openmpi-2.0.2.tar.bz2
                     cd openmpi-2.0.2
