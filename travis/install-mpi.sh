228a043e858f4469920a5fefeb62ee684c3f22a6
edoapra <edoardo.apra@gmail.com>
2020-12-29 14:40:11 -0800

    fix for installation check

--- travis/install-mpi.sh
+++ travis/install-mpi.sh
@@ -37,7 +37,7 @@ case "$os" in
         echo "Linux"
         case "$MPI_IMPL" in
             mpich)
-                if [ ! -d "$TRAVIS_ROOT/mpich" ] || [  ! -x "$1/bin/mpicc" ]; then
+                if [ ! -d "$TRAVIS_ROOT/mpich" ] || [  ! -x "$TRAVIS_ROOT/mpich/bin/mpicc" ]; then
                     wget --no-check-certificate http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
                     tar -xzf mpich-3.2.tar.gz
                     cd mpich-3.2
@@ -76,7 +76,7 @@ EOF
                 fi
                 ;;
             openmpi)
-                if [ ! -d "$TRAVIS_ROOT/open-mpi" ] || [ ! -x "$1/bin/mpicc" ] ; then
+                if [ ! -d "$TRAVIS_ROOT/open-mpi" ] || [ ! -x "$TRAVIS_ROOT/open-mpi/bin/mpicc" ] ; then
                     wget --no-check-certificate https://www.open-mpi.org/software/ompi/v2.0/downloads/openmpi-2.0.2.tar.bz2
                     tar -xjf openmpi-2.0.2.tar.bz2
                     cd openmpi-2.0.2
