bc85484ac1f1227fef5d79643d2f80c503560006
edoapra <edoardo.apra@gmail.com>
2020-12-29 15:15:01 -0800

    clang on linux needs -fPIC

--- travis/build-run.sh
+++ travis/build-run.sh
@@ -59,7 +59,7 @@ case "$os" in
         ;;
     Linux)
 	if [ $(${CC} -dM -E - </dev/null 2> /dev/null |grep __clang__|head -1|cut -c19) ] ; then
-	    CFLAGS+=" -fPIC "
+	    export CFLAGS=$CFLAGS" -fPIC "
 	fi
         echo "Linux CFLAGS" $CFLAGS
         ;;
