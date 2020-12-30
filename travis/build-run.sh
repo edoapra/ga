f7f309411c0694a8026ce15e16cd7e1889e32d09
edoapra <edoardo.apra@gmail.com>
2020-12-29 16:12:38 -0800

    CFLAGS fixes

--- travis/build-run.sh
+++ travis/build-run.sh
@@ -59,7 +59,7 @@ case "$os" in
         ;;
     Linux)
 	if [ $(${CC} -dM -E - </dev/null 2> /dev/null |grep __clang__|head -1|cut -c19) ] ; then
-	    export CFLAGS=$CFLAGS" -fPIC "
+	    export CFLAGS="${CFLAGS} -fPIC "
 	fi
         echo "Linux CFLAGS" $CFLAGS
         ;;
@@ -72,7 +72,7 @@ case "x$PORT" in
         fi
         ;;
     xarmci)
-        ./configure --with-armci=$TRAVIS_ROOT/external-armci CFLAGS=-pthread LIBS=-lpthread
+        ./configure --with-armci=$TRAVIS_ROOT/external-armci CFLAGS="${CFLAGS} -pthread " LIBS=-lpthread
         ;;
     x)
         ./configure ${CONFIG_OPTS}
