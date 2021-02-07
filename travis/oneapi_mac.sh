5eb0f55e6b97e2e0ad6491a2fd31e1ac6ed10268
edoapra <edoardo.apra@gmail.com>
2021-02-06 20:56:43 -0800

    pip

--- travis/oneapi_mac.sh
+++ travis/oneapi_mac.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 brew install python3 pip3
-pip3 install fortran_rt mkl-devel impi-devel
+/usr/local/bin/python3 -m pip install --upgrade pip
+/usr/local/bin/python3 -m pip install fortran_rt 
+/usr/local/bin/python3 -m pip install mkl-devel 
+/usr/local/bin/python3 -m pip install  dpcpp_cpp_rt
 ls -l /opt || true
 ls -l /opt/intel/oneapi || true
 source /opt/intel/oneapi/setvars.sh
