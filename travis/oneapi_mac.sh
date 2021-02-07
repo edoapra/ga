9c05557cfe0d429988e3ef62b2133a26900ce113
edoapra <edoardo.apra@gmail.com>
2021-02-06 16:11:10 -0800

    update

--- travis/oneapi_mac.sh
+++ travis/oneapi_mac.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+mkdir -p ~/mntdmg || true
 cd ~/Downloads
 curl -LJO https://registrationcenter-download.intel.com/akdlm/irc_nas/17426/m_BaseKit_p_2021.1.0.2427_offline.dmg
 curl -LJO https://registrationcenter-download.intel.com/akdlm/irc_nas/17398/m_HPCKit_p_2021.1.0.2681_offline.dmg
 ls -lrt
 #
-hdiutil mount m_BaseKit_p_2021.1.0.2427_offline.dmg
-/Volumes/m_BaseKit_p_2021.1.0.2427_offline/bootstrapper.app/Contents/MacOS/install.sh --cli \
---eula accept --components all --action install
-umount /Volumes/m_BaseKit_p_2021.1.0.2427_offline
+echo "installing BaseKit"
+hdiutil attach m_BaseKit_p_2021.1.0.2427_offline.dmg  -mountpoint ~/mntdmg -nobrowse
+df 
+~/mntdmg/bootstrapper.app/Contents/MacOS/install.sh --cli \
+ --eula accept --components all --action install
+hdiutil detach ~/mntdmg
 #
-hdiutil mount  m_HPCKit_p_2021.1.0.2681_offline.dmg
-/Volumes/m_HPCKit_p_2021.1.0.2681_offline/bootstrapper.app/Contents/MacOS/install.sh --cli \
---eula accept --components all --action install
-umount /Volumes/m_HPCKit_p_2021.1.0.2681_offline
-
+echo "installing HPCKit"
+hdiutil attach m_HPCKit_p_2021.1.0.2681_offline.dmg  -mountpoint ~/mntdmg -nobrowse
+df
+~/mntdmg/bootstrapper.app/Contents/MacOS/install.sh --cli \
+ --eula accept --components all --action install
+hdiutil detach ~/mntdmg
+df
