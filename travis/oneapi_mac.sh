275b410ea9f613ecf9a8e0ef2aac91edf7a353a0
edoapra <edoardo.apra@gmail.com>
2021-02-06 20:24:43 -0800

    pip

--- travis/oneapi_mac.sh
+++ travis/oneapi_mac.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
-mkdir -p ~/mntdmg || true
-cd ~/Downloads
-curl -LJO https://registrationcenter-download.intel.com/akdlm/irc_nas/17426/m_BaseKit_p_2021.1.0.2427_offline.dmg
-curl -LJO https://registrationcenter-download.intel.com/akdlm/irc_nas/17398/m_HPCKit_p_2021.1.0.2681_offline.dmg
-ls -lrt
-#
-echo "installing BaseKit"
-hdiutil attach m_BaseKit_p_2021.1.0.2427_offline.dmg  -mountpoint ~/mntdmg -nobrowse
-df 
-~/mntdmg/bootstrapper.app/Contents/MacOS/install.sh --cli \
- --eula accept --components all --action install
-hdiutil detach ~/mntdmg
-#
-echo "installing HPCKit"
-hdiutil attach m_HPCKit_p_2021.1.0.2681_offline.dmg  -mountpoint ~/mntdmg -nobrowse
-df
-~/mntdmg/bootstrapper.app/Contents/MacOS/install.sh --cli \
- --eula accept --components all --action install
-hdiutil detach ~/mntdmg
-df
+brew install python3 pip3
+pip3 install fortran_rt mkl-devel impi-devel
+ls -l /opt || true
+ls -l /opt/intel/oneapi || true
+source /opt/intel/oneapi/setvars.sh
+
