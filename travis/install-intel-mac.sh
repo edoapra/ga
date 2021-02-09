6a21c7283a55fd5669a86c0fe2506a58ffc6defd
edoapra <edoardo.apra@gmail.com>
2021-02-08 18:22:24 -0800

    mac

--- travis/install-intel-mac.sh
+++ travis/install-intel-mac.sh
@@ -1,50 +1,39 @@
 #!/bin/bash
-#!/bin/bash
 mkdir -p ~/mntdmg || true
 cd ~/Downloads
 curl -LJO https://registrationcenter-download.intel.com/akdlm/irc_nas/17426/m_BaseKit_p_2021.1.0.2427_offline.dmg
 curl -LJO https://registrationcenter-download.intel.com/akdlm/irc_nas/17398/m_HPCKit_p_2021.1.0.2681_offline.dmg
-ls -lrt
+ls -lrt *dmg
 #
 echo "installing BaseKit"
 hdiutil attach m_BaseKit_p_2021.1.0.2427_offline.dmg  -mountpoint ~/mntdmg -nobrowse
 df 
-#sudo ~/mntdmg/bootstrapper.app/Contents/MacOS/install.sh --cli \
-#     --eula accept --components default --action install
 sudo ~/mntdmg/bootstrapper.app/Contents/MacOS/install.sh --cli  --eula accept \
- --action install --components custom:\
-intel.oneapi.mac.basekit.getting_started:\
-intel.oneapi.mac.oneapi-common.vars:\
-intel.oneapi.mac.oneapi-common.licensing:\
-intel.oneapi.mac.dev-utilities:\
-intel.oneapi.mac.tbb.runtime:\
-intel.oneapi.mac.openmp:\
-intel.oneapi.mac.mkl.runtime:\
-intel.oneapi.mac.mkl.devel:\
-intel.oneapi.mac.ipp.runtime:\
-intel.oneapi.mac.basekit.product
-sudo cat /opt/intel/oneapi/logs/* /private/tmp/root/intel_oneapi_installer/*/*log || true
+ --action install --components default
 hdiutil detach ~/mntdmg
 sudo du -sh /opt/intel
 #
 echo "installing HPCKit"
 hdiutil attach m_HPCKit_p_2021.1.0.2681_offline.dmg  -mountpoint ~/mntdmg -nobrowse
 df
-#sudo ~/mntdmg/bootstrapper.app/Contents/MacOS/install.sh --cli \
-# --eula accept --components default --action install
 sudo ~/mntdmg/bootstrapper.app/Contents/MacOS/install.sh --cli  --eula accept \
- --action install --components --components default --action install
-sudo cat /opt/intel/oneapi/logs/* /private/tmp/root/intel_oneapi_installer/*/*log || true
+ --action install --components default
+#sudo cat /opt/intel/oneapi/logs/* /private/tmp/root/intel_oneapi_installer/*/*log || true
 hdiutil detach ~/mntdmg
+sudo rm -rf /opt/intel/oneapi/intelpython /opt/intel/oneapi/dal /opt/intel/oneapi/advisor \
+     /opt/intel/oneapi/ipp /opt/intel/oneapi/conda_channel 	/opt/intel/oneapi/dnnl \
+     /opt/intel/oneapi/installer || true
 df
 ls -lrt /opt ||true
 ls -lrt /opt/intel/oneapi ||true
+sudo du -sh /opt/intel
 source /opt/intel/oneapi/setvars.sh || true
+ifort -V
+icc -V
+#exit 1
 # get user ownership of /opt/intel to keep caching happy
 my_gr=`id -g`
 my_id=`id -u`
 sudo chown -R $my_id /opt/intel
 sudo chgrp -R $my_gr /opt/intel
-ls -Rla /opt/intel/oneapi
-ifort -V 
-icc -V 
+ls -la /opt/intel/oneapi
