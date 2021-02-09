15b3b8fe298e02c5ef0e41db6e96dc4a30ab2ee8
edoapra <edoardo.apra@gmail.com>
2021-02-09 10:49:36 -0800

    merge intel installs

--- travis/install-intel.sh
+++ travis/install-intel.sh
@@ -8,12 +8,44 @@ set -x
 os=`uname`
 
 MAKE_JNUM=4
-
+if [ -f "/opt/intel/oneapi/setvars.sh" ]; then
+    echo "Intel oneapi already installed"
+    source /opt/intel/oneapi/setvars.sh --force || true
+    exit 0
+fi
 
 case "$os" in
     Darwin)
-        echo "Mac not read yet"
-        exit 10
+	mkdir -p ~/mntdmg || true
+	cd ~/Downloads
+	dir_base="17426"
+	dir_hpc="17398"
+	base="m_BaseKit_p_2021.1.0.2427_offline"
+	hpc="m_HPCKit_p_2021.1.0.2681_offline"
+	curl -LJO https://registrationcenter-download.intel.com/akdlm/irc_nas/"$dir_base"/"$base".dmg
+	curl -LJO https://registrationcenter-download.intel.com/akdlm/irc_nas/"$dir_hpc"/"$hpc".dmg
+	echo "installing BaseKit"
+	hdiutil attach "$base".dmg  -mountpoint ~/mntdmg -nobrowse
+	sudo ~/mntdmg/bootstrapper.app/Contents/MacOS/install.sh --cli  --eula accept \
+	     --action install --components default
+	hdiutil detach ~/mntdmg
+	#
+	echo "installing HPCKit"
+	hdiutil attach "$hpc".dmg  -mountpoint ~/mntdmg -nobrowse
+	sudo ~/mntdmg/bootstrapper.app/Contents/MacOS/install.sh --cli  --eula accept \
+	     --action install --components default
+	hdiutil detach ~/mntdmg
+	sudo rm -rf /opt/intel/oneapi/intelpython /opt/intel/oneapi/dal /opt/intel/oneapi/advisor \
+	     /opt/intel/oneapi/ipp /opt/intel/oneapi/conda_channel 	/opt/intel/oneapi/dnnl \
+	     /opt/intel/oneapi/installer /opt/intel/oneapi/vtune_profiler /opt/intel/oneapi/tbb || true
+	source /opt/intel/oneapi/setvars.sh || true
+	ifort -V
+	icc -V
+	# get user ownership of /opt/intel to keep caching happy
+	my_gr=`id -g`
+	my_id=`id -u`
+	sudo chown -R $my_id /opt/intel
+	sudo chgrp -R $my_gr /opt/intel
         ;;
     Linux)
 	export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
