fd27f579d8594f577d7e5d20dd98b18ae826e4b2
edoapra <edoardo.apra@gmail.com>
2026-07-27 16:03:03 -0700

    use armcmi mpi repository from a week ago

--- travis/install-armci-mpi.sh
+++ travis/install-armci-mpi.sh
@@ -35,7 +35,8 @@ ARMCI_MPI_DIR=${TRAVIS_ROOT}/armci-mpi
 /bin/rm -rf ${ARMCI_MPI_DIR}
 git clone -b master --depth 200 https://github.com/jeffhammond/armci-mpi.git ${ARMCI_MPI_DIR}
 cd ${ARMCI_MPI_DIR}
-git checkout 981fefd5b778287dd578d02e4ca70c24fcade9e1
+#git checkout 981fefd5b778287dd578d02e4ca70c24fcade9e1
+git checkout 8ccfcbfd115574fe297068e31b22f3ab8d778bd0
 cd ..
 if ! [ -f ${ARMCI_MPI_DIR}/configure ] ; then
   cd ${ARMCI_MPI_DIR}
